db55023c0e1d1bbcc944909802aff846f135ecde
Deniel Chiang <deniel@jay-creative.com>
2020-02-24 11:37:46 +0800

    add resources image

--- lib/image_upload_demo_web/router.ex
+++ lib/image_upload_demo_web/router.ex
@@ -17,6 +17,7 @@ defmodule ImageUploadDemoWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/images", ImageController
   end
 
   # Other scopes may use custom stacks.
