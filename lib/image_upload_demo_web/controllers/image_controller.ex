4a70a67dcc2fa0edc271ced08ed0a801e1b0398e
Deniel Chiang <deniel@jay-creative.com>
2020-02-24 11:27:15 +0800

    basic controller created

---
+++ lib/image_upload_demo_web/controllers/image_controller.ex
@@ -0,0 +1,16 @@
+defmodule ImageUploadDemoWeb.ImageController do
+  use ImageUploadDemoWeb, :controller
+
+  def index(conn, _params) do
+	render(conn, "index.html")
+  end
+
+  def new(conn, _) do
+	changeset = Image.changeset(%Image{})
+	render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"image" => image_params}) do
+	IO.inspect image_params
+  end
+end
