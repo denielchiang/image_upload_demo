163e27d2d3127346289fc3191b3d93a96126d597
Deniel Chiang <deniel@jay-creative.com>
2020-02-24 11:24:06 +0800

    generate migration

---
+++ priv/repo/migrations/20200224032221_create_images.exs
@@ -0,0 +1,12 @@
+defmodule ImageUploadDemo.Repo.Migrations.CreateImages do
+  use Ecto.Migration
+
+  def change do
+    create table(:images) do
+      add :path, :string
+
+      timestamps()
+    end
+
+  end
+end
